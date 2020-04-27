3a7fd6a0d6696a9df9d0073005e92fdfba7a4046
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-04-27 01:03:46 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,134 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="main-content">
+    <header>
+      <Navbar></Navbar>
+    </header>
+
+    <div class="flex-container">
+      <div class="flex-item">
+        <div class="form-group">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            minima nesciunt impedit voluptas, labore sunt animi optio at numquam
+            ex. Eos, fugit? Vero maiores velit accusantium quaerat accusamus
+            voluptas eaque! Omnis aliquid aspernatur praesentium itaque ex
+            ducimus velit amet officiis aut deleniti facilis rerum molestiae.
+          </p>
+          <form action="" class="form">
+            <input type="email" />
+            <button>Send</button>
+          </form>
+        </div>
+      </div>
+      <div class="flex-item bg-img"></div>
+    </div>
+    <!--  -->
+    <div class="flex-container">
+      <div class="flex-item">
+        <div class="form-group">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            minima nesciunt impedit voluptas, labore sunt animi optio at numquam
+            ex. Eos, fugit? Vero maiores velit accusantium quaerat accusamus
+            voluptas eaque! Omnis aliquid aspernatur praesentium itaque ex
+            ducimus velit amet officiis aut deleniti facilis rerum molestiae.
+          </p>
+          <form action="" class="form">
+            <input type="email" />
+            <button>Send</button>
+          </form>
+        </div>
+      </div>
+      <div class="flex-item bg-img"></div>
+    </div>
+    <div class="flex-container">
+      <div class="flex-item">
+        <div class="form-group">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            minima nesciunt impedit voluptas, labore sunt animi optio at numquam
+            ex. Eos, fugit? Vero maiores velit accusantium quaerat accusamus
+            voluptas eaque! Omnis aliquid aspernatur praesentium itaque ex
+            ducimus velit amet officiis aut deleniti facilis rerum molestiae.
+          </p>
+          <form action="" class="form">
+            <input type="email" />
+            <button>Send</button>
+          </form>
+        </div>
+      </div>
+      <div class="flex-item bg-img"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from "@/components/Navbar.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Navbar },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  width: 100%;
+  font-family: Arial, Helvetica, sans-serif;
+}
+header {
+  width: 100%;
+}
+
+.main-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+}
+
+.flex-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  margin-bottom: 4rem;
+  border-bottom: black 2px solid;
+  max-width: 1200px;
+}
+.flex-item {
+  height: 100%;
+  width: 100%;
+  background-color: rgb(66, 62, 62) (83, 80, 80);
+}
+.bg-img {
+  background-color: rgb(102, 31, 182);
+  width: 100%;
+  height: 100%;
+}
+.form {
+  margin: 1rem 0;
+}
+input {
+  height: 25px;
+  width: 75%;
+}
+button {
+  height: 25px;
+}
+@media screen and (max-width: 768px) {
+  .flex-container {
+    flex-direction: column;
+  }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav id="navbar" class="navbar">
+    <div class="logo">
+      <img class="img-logo" src="@/assets/logo.png" alt="" />
+    </div>
+    <ul class="menu">
+      <li><a href=""></a>Home</li>
+      <li><a href=""></a>About</li>
+      <li><a href=""></a>Contanct</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+  background-color: rgb(51, 51, 51);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 1rem;
+}
+.logo {
+  justify-self: flex-start;
+}
+.img-logo {
+  width: 2rem;
+}
+.menu {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  color: #fff;
+  font-size: 1.2rem;
+}
+.menu li {
+  margin: 0 2rem;
+}
+</style>
